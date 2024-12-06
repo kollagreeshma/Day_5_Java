47822bbb5a2f1e9cebcddf925a8ffd1e686ab21e
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-06 13:44:47 +0530

    Add files via upload

---
+++ patternnum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ class pyramidnum{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=i;j<=5;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
